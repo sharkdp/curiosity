85735cba1b497c62ff661b337f3a27be1058a8b2
sharkdp <davidpeter@web.de>
2016-09-18 15:54:52 +0200

    Add more points to the shape.csv file

--- levels/level009/sample_points.py
+++ levels/level009/sample_points.py
@@ -2,10 +2,10 @@ from random import seed, uniform
 
 seed(1)
 
-for _ in range(1000):
+for _ in range(5000):
     x = uniform(-1, 1)
     y = uniform(-1, 1)
     z = uniform(-1, 1)
 
     if x ** 2 + y ** 2 <= 1:
-        print("{},{},{}".format(x, y, z))
+        print("{}, {}, {}".format(x, y, z))
