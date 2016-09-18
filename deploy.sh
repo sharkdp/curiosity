4b2c416bbb7d521204a806fc7b8709dbd1a2a69d
sharkdp <davidpeter@web.de>
2016-09-18 11:47:43 +0200

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm -rf dist
+mkdir dist
+
+cp -r assets/* assets/.htaccess dist
+
+cat "templates/header.html" "levels/index.html" "templates/footer.html" > "dist/index.html"
+
+for dir in levels/*/; do
+    level=$(basename "$dir")
+    levelDir="dist/$level"
+    mkdir "$levelDir"
+    for file in "$dir"/*; do
+        filename=$(basename "$file")
+        extension="${filename##*.}"
+        if [[ $extension == "html" ]]; then
+            cat "templates/header.html" "$file" "templates/footer.html" > "$levelDir/$filename"
+        else
+            cp "$file" "$levelDir"
+        fi
+    done
+done
