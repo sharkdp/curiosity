75c72a44ec1ecb85cfcdfb097bd0e95cb5d5e097
sharkdp <davidpeter@web.de>
2016-09-19 21:01:32 +0200

    A few dead ends for level 9

--- levels/level009/sample_points.py
+++ levels/level009/sample_points.py
@@ -13,11 +13,11 @@ seed(1)
 for _ in range(5000):
     r_outer = RADIUS
     r_inner = INNER_RADIUS
-    theta_outer = uniform(-2*PI, 2*PI)
-    theta_inner = uniform(-2*PI, 2*PI)
+    theta_outer = uniform(-2 * PI, 2 * PI)
+    theta_inner = uniform(-2 * PI, 2 * PI)
 
     x = math.cos(theta_outer) * (r_outer + math.cos(theta_inner) * r_inner)
     y = math.sin(theta_outer) * (r_outer + math.cos(theta_inner) * r_inner)
     z = math.sin(theta_inner) * r_inner
-    
+
     print("{}, {}, {}".format(x, y, z))
