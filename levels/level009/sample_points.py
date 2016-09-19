b63b7f587b58457915ea4da4d52a1f52dc3b7c6b
Naveen Arunachalam <notatroll.troll@gmail.com>
2016-09-19 12:23:43 -0500

    make level 9 and 10 changes (#5)

--- levels/level009/sample_points.py
+++ levels/level009/sample_points.py
@@ -1,11 +1,23 @@
 from random import seed, uniform
+import math
 
+# CONSTANTS
+RADIUS = 10
+INNER_RADIUS = 3
+PI = math.pi
+
+# INITIALIZE
 seed(1)
 
+# MAIN
 for _ in range(5000):
-    x = uniform(-1, 1)
-    y = uniform(-1, 1)
-    z = uniform(-1, 1)
+    r_outer = RADIUS
+    r_inner = INNER_RADIUS
+    theta_outer = uniform(-2*PI, 2*PI)
+    theta_inner = uniform(-2*PI, 2*PI)
 
-    if x ** 2 + y ** 2 <= 1:
-        print("{}, {}, {}".format(x, y, z))
+    x = math.cos(theta_outer) * (r_outer + math.cos(theta_inner) * r_inner)
+    y = math.sin(theta_outer) * (r_outer + math.cos(theta_inner) * r_inner)
+    z = math.sin(theta_inner) * r_inner
+    
+    print("{}, {}, {}".format(x, y, z))
